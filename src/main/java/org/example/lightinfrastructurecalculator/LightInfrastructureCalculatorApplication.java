e8d83412dc46bfd64ac29886cb10eb0967aeb375
arut3n <arut3ncodes@gmail.com>
2026-04-15 12:40:26 +0200

    git init

---
+++ src/main/java/org/example/lightinfrastructurecalculator/LightInfrastructureCalculatorApplication.java
@@ -0,0 +1,13 @@
+package org.example.lightinfrastructurecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightInfrastructureCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LightInfrastructureCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/lightinfrastructurecalculator/LightInfrastructureCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lightinfrastructurecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightInfrastructureCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
